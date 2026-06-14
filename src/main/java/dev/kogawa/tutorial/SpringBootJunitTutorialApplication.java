288c3a3baef3ad5752e11858e8e0613effcacf47
developer <developer@users.noreply.github.com>
2026-06-14 15:25:49 +0900

    chore: scaffold Spring Boot 4.1 + Java 21 project via Spring Initializr (web, validation, actuator)

---
+++ src/main/java/dev/kogawa/tutorial/SpringBootJunitTutorialApplication.java
@@ -0,0 +1,13 @@
+package dev.kogawa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJunitTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJunitTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kogawa/tutorial/SpringBootJunitTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kogawa.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJunitTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
